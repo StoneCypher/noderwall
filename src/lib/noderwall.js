9760cbe39a9dfd153cc51246b05a7568f3355b20
John Haugeland <stonecypher@gmail.com>
2014-08-11 04:25:09 -0700

    Rudimentary works

--- src/lib/noderwall.js
+++ src/lib/noderwall.js
@@ -1,8 +1,41 @@
 
-;(function() {
+/*jshint node:true*/
+// noderwall.js
 
-    "use strict";
+'use strict';
 
-    console.log('hello, world!');
+var app     = require('commander'),
+    httpreq = require('httpreq');
 
-}());
+app.version('0.0.1')
+   .option('-u, --user [user string]', 'Coderwall username to fetch')
+   .parse(process.argv);
+
+
+
+
+
+function NoderwallFetchUser() {
+
+    if (!(app.user)) {
+        console.log('\nError: Must set a user.\n\n  Usage: noderwall -u JohnHaugeland');
+        return;
+    }
+
+    httpreq.get('http://www.coderwall.com/johnhaugeland.json', function (err, res){
+    
+        if (err) return console.log(err);
+
+        console.log(JSON.parse(res.body));
+
+    });
+
+}
+
+
+
+
+
+exports.noderwall = { user: NoderwallFetchUser };
+
+NoderwallFetchUser();
